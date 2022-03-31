87304f5be503e98f7987fd2952ece250dc279937
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-31 21:40:46 +0300

    wallet routes created

--- server/routes/walletRouter.js
+++ server/routes/walletRouter.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { createWalletController } = require('../controllers/walletController');
+const { createWalletController, getWalletController, updateWalletController, deleteWalletController } = require('../controllers/walletController');
 // middlewares
 const { createWalletMiddleware } = require('../middlewares/walletMiddleware');
+const { authenticateUser } = require('../middlewares/authMiddleware');
 
-router.route('/').post(createWalletMiddleware, createWalletController);
+router.route('/').post([authenticateUser, createWalletMiddleware], createWalletController).get([authenticateUser], getWalletController);
 
+/// reusing the same createWalletMiddleware, because just validating wallet_name
+router.route('/:id').patch([authenticateUser, createWalletMiddleware], updateWalletController).delete([authenticateUser], deleteWalletController);
 module.exports = router;
