ecf13df116a4528739d4c743d042b8714279a048
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-31 21:40:30 +0300

    crud service operations created

--- server/services/walletService.js
+++ server/services/walletService.js
@@ -1,14 +1,15 @@
 const CustomError = require('../errors');
-const { createWallet } = require('../dao/Wallet');
-const { getSingleUser } = require('../dao/Users');
+const { createWallet, getWallet, updateWallet, deleteWallet } = require('../dao/Wallet');
 
-/// creates new wallet if user doesn't have one already
+/**
+ * creates new wallet if user doesn't have one already
+ * checks for already existing wallet by user_id */
 const createWalletService = async Request => {
     const { wallet_name } = Request.body;
 
-    const user = await getSingleUser('user_id', Request.user.user_id);
+    const existingWallet = await getWallet('user_id', Request.user.user_id);
 
-    if (user.length > 0) throw new CustomError.BadRequest('Only one wallet per account is allowed');
+    if (existingWallet.length > 0) throw new CustomError.BadRequest('Only one wallet per account is allowed');
 
     const loweredWalletName = wallet_name.toLowerCase();
 
@@ -20,7 +21,70 @@ const createWalletService = async Request => {
     const wallet = await createWallet(walletPayload);
 
     if (wallet.length <= 0) throw new CustomError.InternalServer('Something went wrong... Please try again later');
-    else return wallet;
+
+    return wallet;
 };
 
-module.exports = { createWalletService };
+/**
+ * returns wallet if one exists
+ * finds the wallet by user_id and returns it
+ * */
+const getWalletService = async reqUser => {
+    const { user_id } = reqUser;
+
+    const wallet = await getWallet('user_id', user_id);
+
+    if (wallet.length <= 0) throw new CustomError.BadRequest('Wallet does not exists. Create new one first');
+
+    return wallet;
+};
+
+/**
+ * returns updated wallet
+ * finds existing wallet by wallet_id, checks is the actual owner made the request and updates it  */
+const updateWalletService = async Request => {
+    const { user_id } = Request.user;
+    const { wallet_name } = Request.body;
+    const { id: walletId } = Request.params;
+
+    // find the wallet by provided wallet id
+    const wallet = await getWallet('wallet_id', walletId);
+
+    // wallet does not exists
+    if (wallet.length <= 0) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    // not the actual user is trying to update his own wallet
+    if (wallet[0].user_id !== user_id) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    // update wallet name
+    const updatedWallet = await updateWallet('wallet_id', walletId, { wallet_name });
+
+    if (updatedWallet.length <= 0) return CustomError.InternalServer('Something went wrong... Please try again later');
+
+    return updatedWallet;
+};
+
+/**
+ * returns true or false after deleting the wallet
+ * finds existing wallet by wallet_id checks is the actual owner made the request and deletes it */
+const deleteWalletService = async Request => {
+    const { user_id } = Request.user;
+    const { id: walletId } = Request.params;
+
+    // find the wallet by provided id
+    const wallet = await getWallet('wallet_id', walletId);
+
+    // wallet does not exists
+    if (wallet.length <= 0) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    // not the actual user is trying to delete his own wallet
+    if (wallet[0].user_id !== user_id) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    const deletedWallet = await deleteWallet('wallet_id', walletId);
+
+    if (!deletedWallet) return CustomError.InternalServer('Something went wrong... Please try again later');
+
+    return { message: 'Wallet successfully deleted' };
+};
+
+module.exports = { createWalletService, getWalletService, updateWalletService, deleteWalletService };
