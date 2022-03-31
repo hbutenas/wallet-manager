ff05e334bdc767a6a0e099eb0de300164cbe34a4
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-31 21:41:29 +0300

    wallet controller which is responsible by calling services

--- server/controllers/walletController.js
+++ server/controllers/walletController.js
@@ -1,2 +1,23 @@
-const createWalletController = async () => {};
-module.exports = { createWalletController };
+const { StatusCodes } = require('http-status-codes');
+const { createWalletService, getWalletService, updateWalletService, deleteWalletService } = require('../services/walletService');
+
+const createWalletController = async (req, res) => {
+    const response = await createWalletService(req);
+    res.status(StatusCodes.CREATED).json({ response });
+};
+
+const getWalletController = async (req, res) => {
+    const response = await getWalletService(req.user);
+    res.status(StatusCodes.OK).json({ response });
+};
+
+const updateWalletController = async (req, res) => {
+    const response = await updateWalletService(req);
+    res.status(StatusCodes.OK).json({ response });
+};
+
+const deleteWalletController = async (req, res) => {
+    const response = await deleteWalletService(req);
+    res.status(StatusCodes.OK).json({ response });
+};
+module.exports = { createWalletController, getWalletController, updateWalletController, deleteWalletController };
