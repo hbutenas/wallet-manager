251bb1c86d9f91859b2cd367eaae6c664aa003f9
Henrikas Butenas <butenashenrikas@gmail.com>
2022-03-31 21:39:47 +0300

    basic crud queries for database

--- server/dao/Wallet.js
+++ server/dao/Wallet.js
@@ -4,4 +4,16 @@ const createWallet = async payload => {
     return database('wallet').insert(payload).returning(['wallet_id', 'wallet_name']);
 };
 
-module.exports = { createWallet };
+const getWallet = async (property, value) => {
+    return database('wallet').where(property, value).returning(['wallet_id', 'wallet_name', 'created_at']);
+};
+
+const updateWallet = async (property, value, payload) => {
+    return database('wallet').where(property, value).update(payload).returning('*');
+};
+
+const deleteWallet = async (property, value) => {
+    return database('wallet').where(property, value).del();
+};
+
+module.exports = { createWallet, getWallet, updateWallet, deleteWallet };
