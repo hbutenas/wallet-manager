aa896238436be002c58de473254ddf581e555e5d
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-02 18:08:21 +0300

    created a check for identifying who is trying to update the wallet

--- server/middlewares/walletMiddleware.js
+++ server/middlewares/walletMiddleware.js
@@ -1,5 +1,5 @@
 const CustomError = require('../errors/');
-
+const { getWallet } = require('../dao/Wallet');
 const createWalletMiddleware = async (req, res, next) => {
     const { wallet_name } = req.body;
 
@@ -16,4 +16,20 @@ const createWalletMiddleware = async (req, res, next) => {
     next();
 };
 
-module.exports = { createWalletMiddleware };
+const identifyWalletOwner = async (req, res, next) => {
+    const { id: walletId } = req.params;
+    const { user_id } = req.user;
+
+    // find the wallet by provided wallet id
+    const wallet = await getWallet('wallet_id', walletId);
+
+    // wallet does not exists
+    if (wallet.length <= 0) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    // not the actual user is trying to update his own wallet
+    if (wallet[0].user_id !== user_id) throw new CustomError.BadRequest(`Wallet with id ${walletId} does not exists`);
+
+    next();
+};
+
+module.exports = { createWalletMiddleware, identifyWalletOwner };
