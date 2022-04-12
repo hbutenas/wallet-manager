127a5a5b552455c31823fe203a08541ff80a4f1a
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-12 20:58:53 +0300

    completed functionallity of router

--- server/routes/balanceRouter.js
+++ server/routes/balanceRouter.js
@@ -2,14 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { createBalanceController, updateBalanceController } = require('../controllers/balanceController');
+const {
+    createBalanceController,
+    updateBalanceController,
+    getBalanceController,
+    deleteBalanceController
+} = require('../controllers/balanceController');
 
 // middlewares
 const { authenticateUser } = require('../middlewares/authMiddleware');
 const { createBalanceMiddleware, updateBalanceMiddleware } = require('../middlewares/balanceMiddleware');
 
 // Sets the amount of money in the balance
-router.route('/').post([authenticateUser, createBalanceMiddleware], createBalanceController);
+// Receive the ballance
+router
+    .route('/')
+    .post([authenticateUser, createBalanceMiddleware], createBalanceController)
+    .get([authenticateUser], getBalanceController)
+    .delete([authenticateUser], deleteBalanceController);
 
 // Makes the action ['increase','decrease']
 router.route('/action').post([authenticateUser, updateBalanceMiddleware], updateBalanceController);
