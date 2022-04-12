6dfe6d397672f6e70514399798700501c4b8dbdf
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-12 20:59:20 +0300

    instead of passing id, now using user_id

--- server/routes/walletRouter.js
+++ server/routes/walletRouter.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // controllers
 const { createWalletController, getWalletController, updateWalletController, deleteWalletController } = require('../controllers/walletController');
 // middlewares
-const { createWalletMiddleware, identifyWalletOwner } = require('../middlewares/walletMiddleware');
+const { createWalletMiddleware } = require('../middlewares/walletMiddleware');
 const { authenticateUser } = require('../middlewares/authMiddleware');
 
-router.route('/').post([authenticateUser, createWalletMiddleware], createWalletController).get([authenticateUser], getWalletController);
-
-/// reusing the same createWalletMiddleware, because just validating wallet_name
 router
-    .route('/:id')
-    .patch([authenticateUser, identifyWalletOwner, createWalletMiddleware], updateWalletController)
-    .delete([authenticateUser, identifyWalletOwner], deleteWalletController);
+    .route('/')
+    .post([authenticateUser, createWalletMiddleware], createWalletController)
+    .get([authenticateUser], getWalletController)
+    .patch([authenticateUser, createWalletMiddleware], updateWalletController)
+    .delete([authenticateUser], deleteWalletController);
+
 module.exports = router;
