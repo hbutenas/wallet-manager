54c62c623f987d492f882db374bd1ae232efcf36
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-14 13:46:21 +0300

    verify-user route for fe

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { registerUserController, loginUserController, logoutUserController } = require('../controllers/authController');
+const { registerUserController, loginUserController, logoutUserController, verifyUserController } = require('../controllers/authController');
 // middlewares
-const { registerUserMiddleware, loginUserMiddleware } = require('../middlewares/authMiddleware');
+const { registerUserMiddleware, loginUserMiddleware, authenticateUser, verifyUserMiddleware } = require('../middlewares/authMiddleware');
 
 router.post('/register', registerUserMiddleware, registerUserController);
 router.post('/login', loginUserMiddleware, loginUserController);
 router.post('/logout', logoutUserController);
+router.post('/verify-user', [authenticateUser], verifyUserController);
 
 module.exports = router;
