e61ede0bbd09040ed2bac7917ba9b629e9c23579
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-14 13:09:21 +0300

    defined basic routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-]
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        children: [
+            {
+                path: '/register',
+                name: 'Register',
+                component: Home // change to normal component
+            },
+            {
+                path: '/forgot',
+                name: 'Forgot',
+                component: Home // change to normal component
+            }
+        ]
+    },
+    {
+        path: '/Dashboard',
+        name: 'Dashboard',
+        component: Home // change to normal component
+    }
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
 
-export default router
+export default router;
+//   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
