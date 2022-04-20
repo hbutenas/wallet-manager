fc2a46c5e0e2edb38b497dead457965414dbebce
Henrikas Butenas <butenashenrikas@gmail.com>
2022-04-20 21:04:30 +0300

    set the default value of amount, because was missing it

--- server/database/migrations/20220328181625_balance.js
+++ server/database/migrations/20220328181625_balance.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
         table.integer('wallet_id').unsigned();
         table.foreign('wallet_id').references('wallet_id').inTable('wallet').onDelete('CASCADE');
         table.enum('action', ['increase', 'decrease', 'created']).defaultTo('created');
-        table.integer('amount');
+        table.integer('amount').defaultTo(0);
         table.integer('total_balance');
         table.timestamp('updated_at').defaultTo(knex.fn.now());
         table.timestamp('created_at').defaultTo(knex.fn.now());
